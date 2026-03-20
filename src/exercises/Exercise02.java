81c15377ced17f476d098cc3ae38c6e741815ea1
ezequias-dev <ezequiasd30@gmail.com>
2026-03-20 10:25:21 -0300

    feat: adiciona exercicio 02 lista de nomes

---
+++ src/exercises/Exercise02.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class Exercise02 {
+
+    public static void main(String[] args) {
+        String[] names = {"Ana", "Pedro", "Maria", "Julia"};
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+    }
+}
